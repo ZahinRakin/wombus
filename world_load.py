a26388d64290d044e7d3a24e47ae50dc373c187b
kibria30 <kibria8007@gmail.com>
2025-07-06 14:01:21 +0600

    file exceptions handled

--- world_load.py
+++ world_load.py
@@ -1,26 +1,32 @@
 class WorldBuilder:
-    def __init__(self, file_path=None, starting_point=None):
-        self.board = self.build(file_path) if file_path else None
-        self.starting_point = starting_point
+    def __init__(self, file_path=None, starting_point=(0, 0)):
+        self.board = self.build(file_path, starting_point)
         
 
-    def build(self, file_path):
-        with open(file_path, 'r') as file:
-            board_str = file.read()
+    def build(self, file_path, starting_point):
+        try:
+            with open(file_path, 'r') as file:
+                board_str = file.read()
 
-            rows = board_str.strip().split('\n')
+                rows = board_str.strip().split('\n')
 
-            board = [[i for i in row] for row in rows]
+                board = [[i for i in row] for row in rows]
 
-            print(len(board), len(board[1]))
+                if (len(board), len(board[1])) != (10, 10):
+                    raise ValueError("Board must be of shape (10, 10)")
 
-            if (len(board), len(board[1])) != (10, 10):
-                raise ValueError("Board must be of shape (10, 10)")
+                board[starting_point[0]][starting_point[1]] = 'A'
 
-        return board
+            return board
+        
+        except Exception as e:
+            print(f"An unexpected error occurred when reading the file!!")
+
+    def print_board(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                print(self.board[i][j], end=' ')
+            print()
     
-    def assign_agent(self):
-        pass
-        
-world = WorldBuilder(file_path='world.txt', starting_point=(0, 0))
-print(world.board)
+world = WorldBuilder(file_path='world.txt', starting_point=(9, 0))
+world.print_board()
