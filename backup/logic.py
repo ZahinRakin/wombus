690cdcebec6e6af79cf92a87ae9199054844d6d7
knah1d <bsse1437@iit.du.ac.bd>
2025-07-16 06:05:43 +0600

    add propositional logic evaluation and conversion functions for CNF

--- backup/logic.py
+++ backup/logic.py
@@ -292,3 +292,194 @@ def tt_true(alpha):
     """
     return tt_entails(TRUE, expr(alpha))
 
+def pl_true(exp, model={}):
+    """Return True if the propositional logic expression is true in the model,
+    and False if it is false. If the model does not specify the value for
+    every proposition, this may return None to indicate 'not obvious';
+    this may happen even when the expression is tautological."""
+    op, args = exp.op, exp.args
+    if exp == TRUE:
+        return True
+    elif exp == FALSE:
+        return False
+    elif is_prop_symbol(op):
+        return model.get(exp)
+    elif op == '~':
+        p = pl_true(args[0], model)
+        if p is None: return None
+        else: return not p
+    elif op == '|':
+        result = False
+        for arg in args:
+            p = pl_true(arg, model)
+            if p is True: return True
+            if p is None: result = None
+        return result
+    elif op == '&':
+        result = True
+        for arg in args:
+            p = pl_true(arg, model)
+            if p is False: return False
+            if p is None: result = None
+        return result
+    p, q = args
+    if op == '>>':
+        return pl_true(~p | q, model)
+    elif op == '<<':
+        return pl_true(p | ~q, model)
+    pt = pl_true(p, model)
+    if pt is None: return None
+    qt = pl_true(q, model)
+    if qt is None: return None
+    if op == '<=>':
+        return pt == qt
+    elif op == '^':
+        return pt != qt
+    else:
+        raise ValueError, "illegal operator in logic expression" + str(exp)
+
+#______________________________________________________________________________
+
+## Convert to Conjunctive Normal Form (CNF)
+
+def to_cnf(s):
+    """Convert a propositional logical sentence s to conjunctive normal form.
+    That is, to the form ((A | ~B | ...) & (B | C | ...) & ...) [p. 253]
+    >>> to_cnf("~(B|C)")
+    (~B & ~C)
+    >>> to_cnf("B <=> (P1|P2)")
+    ((~P1 | B) & (~P2 | B) & (P1 | P2 | ~B))
+    >>> to_cnf("a | (b & c) | d")
+    ((b | a | d) & (c | a | d))
+    >>> to_cnf("A & (B | (D & E))")
+    (A & (D | B) & (E | B))
+    >>> to_cnf("A | (B | (C | (D & E)))")
+    ((D | A | B | C) & (E | A | B | C))
+    """
+    if isinstance(s, str): s = expr(s)
+    s = eliminate_implications(s) # Steps 1, 2 from p. 253
+    s = move_not_inwards(s) # Step 3
+    return distribute_and_over_or(s) # Step 4
+
+def eliminate_implications(s):
+    """Change >>, <<, and <=> into &, |, and ~. That is, return an Expr
+    that is equivalent to s, but has only &, |, and ~ as logical operators.
+    >>> eliminate_implications(A >> (~B << C))
+    ((~B | ~C) | ~A)
+    >>> eliminate_implications(A ^ B)
+    ((A & ~B) | (~A & B))
+    """
+    if not s.args or is_symbol(s.op): return s     ## (Atoms are unchanged.)
+    args = map(eliminate_implications, s.args)
+    a, b = args[0], args[-1]
+    if s.op == '>>':
+        return (b | ~a)
+    elif s.op == '<<':
+        return (a | ~b)
+    elif s.op == '<=>':
+        return (a | ~b) & (b | ~a)
+    elif s.op == '^':
+        assert len(args) == 2   ## TODO: relax this restriction
+        return (a & ~b) | (~a & b)
+    else:
+        assert s.op in ('&', '|', '~')
+        return Expr(s.op, *args)
+
+def move_not_inwards(s):
+    """Rewrite sentence s by moving negation sign inward.
+    >>> move_not_inwards(~(A | B))
+    (~A & ~B)
+    >>> move_not_inwards(~(A & B))
+    (~A | ~B)
+    >>> move_not_inwards(~(~(A | ~B) | ~~C))
+    ((A | ~B) & ~C)
+    """
+    if s.op == '~':
+        NOT = lambda b: move_not_inwards(~b)
+        a = s.args[0]
+        if a.op == '~': return move_not_inwards(a.args[0]) # ~~A ==> A
+        if a.op =='&': return associate('|', map(NOT, a.args))
+        if a.op =='|': return associate('&', map(NOT, a.args))
+        return s
+    elif is_symbol(s.op) or not s.args:
+        return s
+    else:
+        return Expr(s.op, *map(move_not_inwards, s.args))
+
+def distribute_and_over_or(s):
+    """Given a sentence s consisting of conjunctions and disjunctions
+    of literals, return an equivalent sentence in CNF.
+    >>> distribute_and_over_or((A & B) | C)
+    ((A | C) & (B | C))
+    """
+    if s.op == '|':
+        s = associate('|', s.args)
+        if s.op != '|':
+            return distribute_and_over_or(s)
+        if len(s.args) == 0:
+            return FALSE
+        if len(s.args) == 1:
+            return distribute_and_over_or(s.args[0])
+        conj = find_if((lambda d: d.op == '&'), s.args)
+        if not conj:
+            return s
+        others = [a for a in s.args if a is not conj]
+        rest = associate('|', others)
+        return associate('&', [distribute_and_over_or(c|rest)
+                               for c in conj.args])
+    elif s.op == '&':
+        return associate('&', map(distribute_and_over_or, s.args))
+    else:
+        return s
+
+def associate(op, args):
+    """Given an associative op, return an expression with the same
+    meaning as Expr(op, *args), but flattened -- that is, with nested
+    instances of the same op promoted to the top level.
+    >>> associate('&', [(A&B),(B|C),(B&C)])
+    (A & B & (B | C) & B & C)
+    >>> associate('|', [A|(B|(C|(A&B)))])
+    (A | B | C | (A & B))
+    """
+    args = dissociate(op, args)
+    if len(args) == 0:
+        return _op_identity[op]
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return Expr(op, *args)
+
+_op_identity = {'&':TRUE, '|':FALSE, '+':ZERO, '*':ONE}
+
+def dissociate(op, args):
+    """Given an associative op, return a flattened list result such
+    that Expr(op, *result) means the same as Expr(op, *args)."""
+    result = []
+    def collect(subargs):
+        for arg in subargs:
+            if arg.op == op: collect(arg.args)
+            else: result.append(arg)
+    collect(args)
+    return result
+
+def conjuncts(s):
+    """Return a list of the conjuncts in the sentence s.
+    >>> conjuncts(A & B)
+    [A, B]
+    >>> conjuncts(A | B)
+    [(A | B)]
+    """
+    return dissociate('&', [s])
+
+def disjuncts(s):
+    """Return a list of the disjuncts in the sentence s.
+    >>> disjuncts(A | B)
+    [A, B]
+    >>> disjuncts(A & B)
+    [(A & B)]
+    """
+    return dissociate('|', [s])
+
+## Wumpus World example [Fig. 7.13]
+Fig[7,13] = expr("(B11 <=> (P12 | P21))  &  ~B11")
+
