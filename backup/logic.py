aee371bf04d31dec68e187500fb2a63a1e0a9e1a
knah1d <bsse1437@iit.du.ac.bd>
2025-07-16 06:04:46 +0600

    add PropKB class for propositional logic with methods for tell, ask_generator, and retract

--- backup/logic.py
+++ backup/logic.py
@@ -59,4 +59,110 @@ class KB:
         "Remove sentence from the KB."
         abstract
 
+class PropKB(KB):
+    "A KB for propositional logic. Inefficient, with no indexing."
 
+    def __init__(self, sentence=None):
+        self.clauses = []
+        if sentence:
+            self.tell(sentence)
+
+    def tell(self, sentence):
+        "Add the sentence's clauses to the KB."
+        self.clauses.extend(conjuncts(to_cnf(sentence)))
+
+    def ask_generator(self, query):
+        "Yield the empty substitution if KB implies query; else nothing."
+        if tt_entails(Expr('&', *self.clauses), query):
+            yield {}
+
+    def retract(self, sentence):
+        "Remove the sentence's clauses from the KB."
+        for c in conjuncts(to_cnf(sentence)):
+            if c in self.clauses:
+                self.clauses.remove(c)
+
+#______________________________________________________________________________
+
+class Expr:
+    """A symbolic mathematical expression.  We use this class for logical
+    expressions, and for terms within logical expressions. In general, an
+    Expr has an op (operator) and a list of args.  The op can be:
+      Null-ary (no args) op:
+        A number, representing the number itself.  (e.g. Expr(42) => 42)
+        A symbol, representing a variable or constant (e.g. Expr('F') => F)
+      Unary (1 arg) op:
+        '~', '-', representing NOT, negation (e.g. Expr('~', Expr('P')) => ~P)
+      Binary (2 arg) op:
+        '>>', '<<', representing forward and backward implication
+        '+', '-', '*', '/', '**', representing arithmetic operators
+        '<', '>', '>=', '<=', representing comparison operators
+        '<=>', '^', representing logical equality and XOR
+      N-ary (0 or more args) op:
+        '&', '|', representing conjunction and disjunction
+        A symbol, representing a function term or FOL proposition
+
+    Exprs can be constructed with operator overloading: if x and y are Exprs,
+    then so are x + y and x & y, etc.  Also, if F and x are Exprs, then so is
+    F(x); it works by overloading the __call__ method of the Expr F.  Note
+    that in the Expr that is created by F(x), the op is the str 'F', not the
+    Expr F.   See http://www.python.org/doc/current/ref/specialnames.html
+    to learn more about operator overloading in Python.
+
+    WARNING: x == y and x != y are NOT Exprs.  The reason is that we want
+    to write code that tests 'if x == y:' and if x == y were the same
+    as Expr('==', x, y), then the result would always be true; not what a
+    programmer would expect.  But we still need to form Exprs representing
+    equalities and disequalities.  We concentrate on logical equality (or
+    equivalence) and logical disequality (or XOR).  You have 3 choices:
+        (1) Expr('<=>', x, y) and Expr('^', x, y)
+            Note that ^ is bitwose XOR in Python (and Java and C++)
+        (2) expr('x <=> y') and expr('x =/= y').
+            See the doc string for the function expr.
+        (3) (x % y) and (x ^ y).
+            It is very ugly to have (x % y) mean (x <=> y), but we need
+            SOME operator to make (2) work, and this seems the best choice.
+
+    WARNING: if x is an Expr, then so is x + 1, because the int 1 gets
+    coerced to an Expr by the constructor.  But 1 + x is an error, because
+    1 doesn't know how to add an Expr.  (Adding an __radd__ method to Expr
+    wouldn't help, because int.__add__ is still called first.) Therefore,
+    you should use Expr(1) + x instead, or ONE + x, or expr('1 + x').
+    """
+
+    def __init__(self, op, *args):
+        "Op is a string or number; args are Exprs (or are coerced to Exprs)."
+        assert isinstance(op, str) or (isnumber(op) and not args)
+        self.op = num_or_str(op)
+        self.args = map(expr, args) ## Coerce args to Exprs
+
+    def __call__(self, *args):
+        """Self must be a symbol with no args, such as Expr('F').  Create a new
+        Expr with 'F' as op and the args as arguments."""
+        assert is_symbol(self.op) and not self.args
+        return Expr(self.op, *args)
+
+    def __repr__(self):
+        "Show something like 'P' or 'P(x, y)', or '~P' or '(P | Q | R)'"
+        if not self.args:         # Constant or proposition with arity 0
+            return str(self.op)
+        elif is_symbol(self.op):  # Functional or propositional operator
+            return '%s(%s)' % (self.op, ', '.join(map(repr, self.args)))
+        elif len(self.args) == 1: # Prefix operator
+            return self.op + repr(self.args[0])
+        else:                     # Infix operator
+            return '(%s)' % (' '+self.op+' ').join(map(repr, self.args))
+
+    def __eq__(self, other):
+        """x and y are equal iff their ops and args are equal."""
+        return (other is self) or (isinstance(other, Expr)
+            and self.op == other.op and self.args == other.args)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        "Need a hash method so Exprs can live in dicts."
+        return hash(self.op) ^ hash(tuple(self.args))
+
+   
